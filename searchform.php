1057cf0abd311217f42e481d65e9fc74233b64bb
Dierreweb <webdr87@gmail.com>
2021-01-02 19:11:16 +0100

    modifiche a searchform e sidebar

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
   */
 ?>
 
-<div class="search new-search">
+<div class="search">
   <form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
     <input type="search" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'dr' ); ?>" required/>
     <button class="search-submit" type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'dr' ); ?>">
